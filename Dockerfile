9cc9efe12779b2847454d0bc59ab08ea9b4fce96
Valentin Podkamennyi <vpodk@users.noreply.github.com>
2024-02-06 09:29:28 -0800

    Initial API setup

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+# https://nodejs.org/en/docs/guides/nodejs-docker-webapp/
+FROM node:20
+
+# Create app directory
+WORKDIR /opt/wiseblend
+
+# Install app dependencies
+RUN apt-get -y update
+RUN apt-get -y upgrade
+RUN apt-get -y install chromium
+COPY package*.json ./
+RUN npm install
+RUN npm ci --only=production
+
+# Bundle app source
+COPY . .
+RUN npm run build
+
+EXPOSE 4000
+CMD ["npm", "start"]
---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const compression = require('compression');
+const cors = require('cors');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.disable('x-powered-by');
+app.use(compression({threshold: 0}));
+app.use(cors());
+app.use('*', (req, res, next) => {
+  res.header('Content-Type', 'application/json; charset=utf-8');
+  next();
+});
+
+app.get('*', (req, res) => {
+  res.send('{"error": "Bad Request"}');
+});
+
+app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
